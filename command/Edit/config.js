2bd0378a65767129a86a47ace5d40fbf7f95b63e
rafa129000 <114792580+rafa129000@users.noreply.github.com>
2022-10-13 10:49:40 +0700

    Update config.js

--- command/Edit/config.js
+++ command/Edit/config.js
@@ -1,12 +1,12 @@
 const { modul } = require('../Edit/module');
 const { chalk, fs, proces } = modul;
 
-global.ownerNumber = ["6282399255216@s.whatsapp.net"]
-global.namabotnya = 'Cyber Is Real'
-global.namaownernya = 'Cyber Executed'
-global.packname = '\n\n\n\n\n\n\n\n\n\n© Cyber||+62🇲🇨\nI`m From Indonesia\n\n\n\n\n\n\n\n\n\n'
+global.ownerNumber = ["6289507348822@s.whatsapp.net"]
+global.namabotnya = '𝐑𝐀𝐏𝐙𝐙 𝐄𝐗𝐄𝐂𝐔𝐓𝐄𝐃'
+global.namaownernya = '𝐑𝐀𝐏𝐙𝐙'
+global.packname = '\n\n\n\n\n\n\n\n\n\n© Rapz||+62🇲🇨\nI`m From Indonesia\n\n\n\n\n\n\n\n\n\n'
 global.author = '\n\n\n\n\n\n\n\n\n\nWa : 0823-9925-5216\nYT : Cyber S-C-D-B\n\n\n\n\n\n\n\n\n\n'
-global.sessionName = 'CyberExe'
+global.sessionName = 'RapzzExe'
 global.lolkey = 'Atakbotz'
 global.dapkey = 'Kirbotz123'
 global.kirkey = 'KirBotz'
@@ -15,12 +15,12 @@ global.group = 'https://bit.ly/38oNU0D'
 global.youtube = 'https://bit.ly/39Ivus6'
 global.website = 'https://bit.ly/3yk3gOo'
 global.github = 'https://bit.ly/3FqVBj8'
-global.noown = 'https://wa.me/6282399255216'
+global.noown = 'https://wa.me/6289507348822'
 global.region = 'I`m From Indonesia'
-global.prefa = ['','!','.','#','-','�?']
+global.prefa = ['','!','.','#','-','�?']
 global.api = 
 {
-    success: '```Success✅```',
+    success: '```Sukses dek✅```',
     admin: '```Fitur Khusus Admin Group!!!```',
     botAdmin: '```Bot Harus Menjadi Admin Terlebih Dahulu!!!```',
     owner: '```Fitur Khusus Owner Bot!!!```',
@@ -30,7 +30,7 @@ global.api =
     error: '```Mungkin Lagi Error Kak Harap Lapor Owner Biar Langsung Di Benerin🙏```',
     wait: '```Sabar Jancok```',
     ban: '_Maaf Kamu Sudah Di Ban Silahkan Chat Owner Di Bawah Untuk Membuka Nya_',
-    verif: '_Maaf Kak Kakak Belum Jadi User CΨβΣR ΣXΣCUTΣD Silahkan Register Terlebih Dahulu Click Di Bawah_'
+    verif: '_Maaf Kak Kakak Belum Jadi User rapzz ΣXΣCUTΣD Silahkan Register Terlebih Dahulu Click Di Bawah_'
 }
 
 global.thumb = fs.readFileSync('./command/Image/thumb.jpg')
